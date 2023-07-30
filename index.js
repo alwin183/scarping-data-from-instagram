b2917a04e6d7fc3d1aa878b0000c15c71f06b7bf
AlwinL <alwin@refactory.id>
2023-07-30 23:05:12 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const PORT = 8000;
+const puppeteer = require("puppeteer");
+const express = require('express');
+
+const app = express();
+
+const getInstagramData = async () => {
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  await page.goto(
+    "https://www.instagram.com/p/CvHYg1QvRMj/?utm_source=ig_web_copy_link",
+    {
+      waitUntil: "networkidle2",
+    }
+  );
+
+  const title = await page.evaluate(() =>
+    document
+      .querySelector("meta[property='og:title']")
+      .getAttribute("content")
+  );
+  const image = await page.evaluate(() =>
+    document
+      .querySelector("meta[property='og:image']")
+      .getAttribute("content")
+  );
+
+  await browser.close();
+
+  return { title, image };
+};
+
+app.get('/instagram', async (req, res) => {
+  try {
+    const { title, image } = await getInstagramData();
+    console.log(title);
+    console.log(image);
+
+    res.json({
+      title,
+      image
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Error occurred while fetching data" });
+  }
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
