27b76dd104cfe4f7f73fa2c54d6805a207c52ead
AlwinL <alwin@refactory.id>
2023-08-02 16:30:34 +0700

    add path/health

--- index.js
+++ index.js
@@ -4,38 +4,64 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-app.use(cors())
+app.use(cors());
 
-
-const getInstagramData = async () => {
-  const browser = await puppeteer.launch({ headless: true });
-  const page = await browser.newPage();
-  await page.goto(
-    "https://www.instagram.com/p/CvHYg1QvRMj/?utm_source=ig_web_copy_link",
-    {
-      waitUntil: "networkidle2",
-    }
-  );
-
-  const title = await page.evaluate(() =>
-    document
-      .querySelector("meta[property='og:title']")
-      .getAttribute("content")
-  );
-  const image = await page.evaluate(() =>
-    document
-      .querySelector("meta[property='og:image']")
-      .getAttribute("content")
-  );
-
-  await browser.close();
-
-  return { title, image };
+const checkSocialMediaLink = (link) => {
+  if (link.includes("instagram.com/")) {
+    return "Instagram";
+  } else if (link.includes("facebook.com/")) {
+    return "Facebook";
+  } else {
+    return "Unknown social media platform";
+  }
 };
 
-app.get('/instagram', async (req, res) => {
+const getData = async (link) => {
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  let title = "";
+  let image = "";
+
+  const socialMediaPlatform = checkSocialMediaLink(link);
+
+  if (socialMediaPlatform === 'Instagram' || socialMediaPlatform === 'Facebook') {
+    await page.goto(link, { waitUntil: "networkidle2" });
+
+    title = await page.evaluate(() =>
+      document
+        .querySelector("meta[property='og:title']")
+        .getAttribute("content")
+    );
+    image = await page.evaluate(() =>
+      document
+        .querySelector("meta[property='og:image']")
+        .getAttribute("content")
+    );
+
+    await browser.close();
+  }
+
+  return {
+    title,
+    image
+  }
+};
+
+app.get('/health', async (req, res) => {
   try {
-    const { title, image } = await getInstagramData();
+    res.json({
+      msg: "serverUp!!!",
+      status: 200,
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Error occurred while fetching data" });
+  }
+});
+
+app.get('/get_link_data', async (req, res) => {
+  try {
+    const link = req.query.link; // Use req.query to get the link from the URL query string
+    const { title, image } = await getData(link);
     console.log(title);
     console.log(image);
 
@@ -47,5 +73,6 @@ app.get('/instagram', async (req, res) => {
     res.status(500).json({ error: "Error occurred while fetching data" });
   }
 });
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
